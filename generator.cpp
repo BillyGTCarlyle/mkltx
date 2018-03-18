537759a088647fa8446f07d9bf7b25c133cedfd6
Billy Carlyle <billy@billycarlyle.uk>
2018-03-18 12:48:22 +0000

    Added some packages, cleaned up a bit

--- generator.cpp
+++ generator.cpp
@@ -8,19 +8,25 @@ void MakeDocument(char* type, char* title){
 	Type.assign(type);
 	Title.assign(title);
 	if(Type == "s"){
-		std::ofstream output(Title+".tex", std::ofstream::out);
+		system(("mkdir "+Title).c_str());
+		std::ofstream output(Title+"/"+Title+".tex", std::ofstream::out);
 		output << "\\documentclass[a4paper]{article} \n";
 		output << "\\author{Billy Carlyle} \n";
 		output << "\\usepackage{graphicx} \n";
+		output << "\\usepackage{float} \n";
+		output << "\\usepackage{cite} \n";
 		output << "\\date{\\today} \n";
 		output << "\\begin{document} \n";
 		output << "\\maketitle \n";
+		output << "\\newpage \n";
 		output << "\\tableofcontents \n";
+		output << "\\newpage \n";
 		output << "\n\\end{document}";
 		output.close();
 		std::cout << "Standard document " << Title << ".tex generated..." << std::endl;
 	}else if(Type == "p"){
-		std::ofstream output(Title+".tex", std::ofstream::out);	
+		system(("mkdir "+Title).c_str());
+		std::ofstream output(Title+"/"+Title+".tex", std::ofstream::out);	
 		output << "\\documentclass{beamer} \n";
 		output << "\\usepackage{graphicx} \n";
 		output << "\\author{Billy Carlyle} \n";
